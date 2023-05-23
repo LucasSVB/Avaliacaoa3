c89c26ef2df3c975debc947528f234629c89d6db
LucasSVB <127763852+LucasSVB@users.noreply.github.com>
2023-05-23 11:42:18 -0300

    Add files via upload

---
+++ conexão.php
@@ -0,0 +1,8 @@
+<?php
+$conexão=mysqli_connect ('localhost','root','','banco3','3306'
+);
+if(!$conexão){
+    die('banco de dados em manutenção');
+} 
+
+?>
---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+, initial-scale=1.0">
+    <title>Tela de login</title>
+</head>
+<body>
+    <form id="login" action= "logado.php" method= "Post">
+        login: <input type="text" name= "login"><br>
+        senha: <input type="password" name= "senha"><br>
+        <input type="submit" name="entrar" value="entrar">
+
+    </form>
+</body>
+</html>
