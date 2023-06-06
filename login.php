967d8a585630269b0ae697fae26409de2d01d3a3
LucasSVB <127763852+LucasSVB@users.noreply.github.com>
2023-06-06 11:01:12 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,26 @@
 , initial-scale=1.0">
     <title>Tela de login</title>
 </head>
+<head>
 <body>
     <form id="login" action= "logado.php" method= "Post">
-        login: <input type="text" name= "login"><br>
-        senha: <input type="password" name= "senha"><br>
-        <input type="submit" name="entrar" value="entrar">
+        Login: <input type="text" name= "login"><br>
+        Senha: <input type="password" name= "senha"><br>
+        <input type="submit" name="Entrar" value="Entrar">
 
     </form>
+
+    <style>
+    
+    html, body {
+      height: 100%;
+      margin: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    </style>
+    
 </body>
+</head>
 </html>
