29cc56919e8832bc4683e803c75eb6ac8fd02515
LucasSVB <127763852+LucasSVB@users.noreply.github.com>
2023-06-06 11:04:17 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,11 +1,11 @@
 <html>
     <body>
         <center>
-        <h1>Cadastro</h1>
+        <h1>Faça seu cadastro</h1>
         <form id="Cadastro" action="cadastrar.php" method="POST">
-            nome: <input type = "text" name= "nome" required><br>
-            login: <input type = "text" name= "login" required><br>
-            senha: <input type = "password" name= "senha" required><br><br>
+            Nome: <input type = "text" name= "nome" required><br>
+            Login: <input type = "text" name= "login" required><br>
+            Senha: <input type = "password" name= "senha" required><br><br>
             <input type = "submit" name = "cadastrar" value = "cadastrar">
             <p>
             <a href="login.php">Voltar</a><br>
