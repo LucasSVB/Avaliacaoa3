dd8dfa42205616ecab2e7c81f629297a33774f73
LucasSVB <127763852+LucasSVB@users.noreply.github.com>
2023-06-14 23:48:30 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,33 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> Projeto A3 </title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			background-color: blanchedalmond;
+		}
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=
-, initial-scale=1.0">
-    <title>Tela de login</title>
+		center {
+			margin-top: 100px;
+		}
+
+		h1 {
+			color: black;
+		}
+
+		form {
+			width: 400px;
+			padding: 40px;
+			background-color: gray;
+			border-radius: 100px;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+		}
+
+		input[type="text"],
+		input[type="password"] {
+			width: 50%;
+			padding: 10px;
+			margin-bottom: 10px;
+			border: 1px solid #ccc;
+			border-radius: 50px;
+			box-sizing: border-box;
+		}
+
+		input[type="submit"] {
+			width: 30%;
+			padding: 10px;
+			background-color: gainsboro;
+			color: black;
+			border: none;
+			border-radius: 10px;
+			cursor: pointer;
+		}
+
+		input[type="submit"]:hover {
+			background-color: paleturquoise;
+		}
+
+		a {
+			color: greenyellow;
+			text-decoration: none;
+		}
+
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
 </head>
-<head>
 <body>
-    <form id="login" action= "logado.php" method= "Post">
-        Login: <input type="text" name= "login"><br>
-        Senha: <input type="password" name= "senha"><br>
-        <input type="submit" name="Entrar" value="Entrar">
-
-    </form>
-
-    <style>
-    
-    html, body {
-      height: 100%;
-      margin: 0;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-    </style>
-    
+	<center>
+		<h1>Login</h1>
+		<form id="form-login" action="logado.php" method="POST">
+			Login: <input type="text" name="login" required><br>
+			Senha: <input type="password" name="senha" required><br><br>
+			<input type="submit" name="entrar" value="Entrar">
+			<p>Não tem conta?, Crie uma conta <a href="cadastro.php">aqui!</a></p>
+			<p><a href="alterarsenha2.php">Esqueceu a senha?</a></p>
+		</form>
+	</center>
 </body>
-</head>
 </html>
